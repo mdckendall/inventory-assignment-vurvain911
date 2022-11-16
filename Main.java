68e3c16bfa8f3500c3ef2d59f4fb0025f84e825b
DanielLosa <113356754+vurvain911@users.noreply.github.com>
2022-11-16 12:17:33 +0000

    Added inventory java

--- Main.java
+++ Main.java
@@ -1,12 +1,5 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-class Inventory {
-
-}
-
 class Main {
-	public static void main(String[] args) {
-
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
