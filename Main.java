8159c3d8afd1e79b565ecf3374b16bfd0b97adaa
DanielLosa <113356754+vurvain911@users.noreply.github.com>
2022-11-20 22:29:32 +0000

    e

--- Main.java
+++ Main.java
@@ -65,7 +65,7 @@ class Main {
       {
         for(int a = 0; a < Storage.size(); a++)
         {
-          System.out.println(Storage.get(a).getName() + ", " + Storage.get(a).getSerialNum() + ", $" + Storage.get(a).getValue());    
+          System.out.println(Storage.get(a).getName() + ", " + Storage.get(a).getSerialNum() + ", " + Storage.get(a).getValue());    
         }
       }
       else if(userInput == 5)
